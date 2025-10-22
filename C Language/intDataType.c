e4419f6b5b86f583868f9e8df127bc30660dfcb5
Rakesk-Kumar0 <rakeshk49312@gmail.com>
2025-10-23 00:03:08 +0530

    adding new file

---
+++ C Language/intDataType.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x;
+    int y;
+    x = 5;
+    y = 6;
+    int z = x+y;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    printf("%d",z);
+    return 0;
+}

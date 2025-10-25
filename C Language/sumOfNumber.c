ca0b8cbc71cf353c6d076350dd1091e74f2a4568
Rakesk-Kumar0 <rakeshk49312@gmail.com>
2025-10-25 20:12:55 +0530

    adding new file

---
+++ C Language/sumOfNumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x,y,sum;
+    printf("Enter first number:");
+    scanf("%d",&x);
+     printf("Enter second number:");
+    scanf("%d",&y);
+    sum = x+y;
+    printf("sum of numbers = %d",sum);
+    return 0;
+}

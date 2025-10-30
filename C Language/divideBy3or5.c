a4674b7e77fd880cd68a1a7c9e9ee2ee07046dc1
Rakesk-Kumar0 <rakeshk49312@gmail.com>
2025-10-30 23:26:40 +0530

    adding new file

---
+++ C Language/divideBy3or5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number ");
+    scanf("%d",&n);
+    
+
+    if(n%5==0 || n%3==0){
+         printf(" the given number is divisible by 5 or 3 ");
+    
+    }else 
+      printf(" the given number is not divisible by 5 or 3 ");
+    
+    return 0;
+
+}

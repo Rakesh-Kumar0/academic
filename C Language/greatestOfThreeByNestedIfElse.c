36d7827ec0beda273ad232aea6d4a4d4b47b4baf
Rakesk-Kumar0 <rakeshk49312@gmail.com>
2025-10-31 22:02:32 +0530

    adding new file

---
+++ C Language/greatestOfThreeByNestedIfElse.c
@@ -0,0 +1,26 @@
+include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter value of a ,b and c ");
+    scanf("%d%d%d",&a,&b,&c);
+    
+
+    if(a>b){// b is out of race
+     if(a>c){
+         printf(" the greatest is %d ",a);
+     }else{//c>a -> b<a<c
+          printf("the greatest is %d",c);
+     }
+    }
+    else{ //b>a -> a ab sabse bada nahi hai
+        if(b>c){
+          printf("the greatest is %d",b);
+        } else{ //b<c
+          printf("the greatest is %d",c);
+        } 
+
+    }
+    
+    return 0;
+
+}

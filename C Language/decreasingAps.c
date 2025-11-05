a9e0eba368a68d9c7fb5cd91a40b2b080d8c8018
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2025-11-05 21:14:37 +0530

    adding new file

---
+++ C Language/decreasingAps.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    // int n;
+    // printf("Enter number ");
+    // scanf("%d",&n);
+    
+    int a = 100;
+    for(int i=1;a>0;i=i+1){
+        printf("%d\n",a);
+        a = a-3;
+
+    }
+    return 0;
+}

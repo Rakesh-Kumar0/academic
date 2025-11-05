7df2ab13222b1e881b7cc6435bf689fc568b06ce
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2025-11-05 21:16:06 +0530

    adding new file

---
+++ C Language/evenOddInOne2Hundred.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    // for(int i=2;i<=100;i=i+2){
+    //     printf("%d\n",i);
+    // }
+    for(int i=1;i<=100;i++){
+        // if(i%2!=0){
+        //     printf("%d",i);
+        // }
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+
+}

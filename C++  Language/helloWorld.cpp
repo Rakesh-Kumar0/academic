7c88174cd67a808455f0e89cd6dd5260d5306509
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-02-02 18:35:43 +0530

    adding new file

---
+++ C++  Language/helloWorld.cpp
@@ -0,0 +1,14 @@
+// This is header which is used to use input output function
+#include<iostream>
+using namespace std;
+
+// this is driven function code will start execute from this function
+int main(){
+    // cout is used to display output 
+    // endl is use to go in next line
+
+    cout<<"Hello World !!" <<endl; 
+    // return is used to come out from the function
+    
+    return 0;
+}
